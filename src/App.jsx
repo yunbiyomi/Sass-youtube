78412a6ae790733bae61174680152ff489aa4c5f
yunbiyomi <yunbiyomi@naver.com>
2023-12-04 23:59:28 +0900

    ✨ Feat : router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import SearchedVideosPage from './pages/SearchedVideosPage'
+import VideoPage from './pages/VideoPage'
+
+const Layout = () => {
+  return(
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-      <p>Hello world!</p>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path='/results/:input' element={<SearchedVideosPage />} />
+          <Route path='/video/:videoId' element={<VideoPage />} />
+        </Route>
+      </Routes>
     </>
   )
 }
