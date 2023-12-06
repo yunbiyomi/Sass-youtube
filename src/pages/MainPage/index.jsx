5093acf3a5f436d0cc54958c2a8e9a0e7a1c0566
yunbiyomi <yunbiyomi@naver.com>
2023-12-06 23:50:17 +0900

    ✨ Feat : googleapis를 이용한 youtube API 가져오기

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import axios from '../../api/axios'
+import React, { useCallback, useEffect, useState } from 'react'
+import { getVideoInfo } from '../../helpers/fetchingData';
 
 const MainPage = () => {
+  const storedVideos = JSON.parse(localStorage.getItem('mainVideos'));
+  const [mainVideos, setMainVideos] = useState(storedVideos || []);
+
+  const getMainVideos = useCallback(async() => {
+    try {
+      if(!storedVideos) {
+        const response = await axios.get(`/search?part=snippet&maxResults=10&q=playlist`);
+        let videosArray = await response.data.items;
+        videosArray = await getVideoInfo(videosArray);
+        setMainVideos(videosArray);
+
+        localStorage.setItem('mainVideos', JSON.stringify(videosArray));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [storedVideos])
+
+  useEffect(() => {
+    getMainVideos();
+  }, [getMainVideos])
+
   return (
     <div>MainPage</div>
   )
